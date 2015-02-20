63ab92aac2019afa9845e7676df19a51cbce2024
King Yu Law <king_yu_law@hotmail.com>
2015-02-20 13:43:43 +1300

    Updated Functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Creates a list that allows users to get and set
+## a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## Stores the inverse of the matrix in a variable
+  invertedMatrix <- NULL
+  
+  ## Sets the matrix
+  setMatrix <- function(matrixInput) {
+    x <<- matrixInput
+    invertedMatrix <<- NULL
+  }
+  
+  ## Gets the matrix
+  getMatrix <- function() x
+  
+  ## Sets the inverse of the matrix
+  setInvertedMatrix <- function(invertedMatrixInput) invertedMatrix <<- invertedMatrixInput
+  
+  ## Gets the inverse of the matrix
+  getInvertedMatrix <- function() invertedMatrix
+  
+  ## Returns the list of functions
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvertedMatrix = setInvertedMatrix,
+       getInvertedMatrix = getInvertedMatrix)
+  
 }
 
-
-## Write a short comment describing this function
-
+## Accepts the list created by the makeCacheMatrix
+## function and returns the inverse of the matrix
+## stored in the list. Also stores the inverse of the
+## matrix back into the list
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Gets the inverse of the matrix
+  invertedMatrix <- x$getInvertedMatrix()
+  
+  ## Checks whether the inverse of the matrix
+  ## exists. If it exists, returns it.
+  if(!is.null(invertedMatrix)) {
+    message("getting cached data")
+    return(invertedMatrix)
+  }
+  
+  ## Gets the matrix
+  matrix <- x$getMatrix()
+  
+  ## Calculates the inverse of the matrix
+  invertedMatrix <- solve(matrix)
+  
+  ## Stores the inverse of the matrix back into the list
+  x$setInvertedMatrix(invertedMatrix)
+  
+  ## Returns the inverse of the matrix to users
+  invertedMatrix
+  
 }
